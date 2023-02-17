c2d64053e91e7496d15dd33f6b15768abefd9d66
Moshood Olawale <shabimoshood07@gmail.com>
2023-02-17 22:01:47 +0100

    modified code

--- controllers/user.js
+++ controllers/user.js
@@ -8,44 +8,70 @@ const getAllUsers = async (req, res) => {
 };
 
 // Add comment
-const addComment = async (req, res) => {
-  const { artisanId, userId } = req.params;
-  // const { userId, role } = req.user;
+const addComment = async (req, res, next) => {
+  const { artisanId } = req.params;
   let commentBy;
-  if (req.user) {
-    const user = await User.findById(req.user.userId);
-    commentBy = user.username;
-  } else if (req.artisan) {
-    const artisan = await Artisan.findById(req.artisan.artisanId);
-    commentBy = artisan.businessName;
-  }
 
-  const artisan = await Artisan.findByIdAndUpdate(
-    { _id: artisanId },
-    { $addToSet: { comments: { ...req.body, createdBy: commentBy } } },
-    { new: true, runValidators: true }
-  );
-  res.json({ msg: "comment sent successfuly" });
+  try {
+    if (req.user) {
+      const user = await User.findById(req.user.userId);
+      commentBy = user.username;
+    } else if (req.artisan) {
+      if (req.artisan.artisanId == artisanId) {
+        const error = new Error("You can not leave a comment on your page");
+        error.statusCode = 405;
+        throw error;
+      }
+      const artisan = await Artisan.findById(req.artisan.artisanId);
+      commentBy = artisan.businessName;
+    }
+
+    const artisan = await Artisan.findByIdAndUpdate(
+      { _id: artisanId },
+      { $addToSet: { comments: { ...req.body, createdBy: commentBy } } },
+      { new: true, runValidators: true }
+    );
+    res.status(200).json({ msg: "comment sent successfuly" });
+  } catch (error) {
+    next(error);
+  }
 };
 
 // merger like and unlike routes by using toggles
 
 // Like comment
-const addLikes = async (req, res) => {
-  console.log(req.user);
-  const { commentId, userId } = req.params;
+const addLikes = async (req, res, next) => {
+  const { commentId } = req.params;
+  let likeById;
+
+  if (req.user) {
+    likeById = req.user.userId;
+  } else if (req.artisan) {
+    likeById = req.artisan.artisanId;
+  }
 
   try {
     const findArtisan = await Artisan.findOne({
       "comments.commentId": commentId,
     });
 
+    if (!findArtisan) {
+      throw new Error("No Artisan Found");
+    }
+
+    if (findArtisan._id == likeById) {
+      throw new Error("You can not like a comment on your page");
+    }
     const comment = findArtisan.comments.filter(
       (comm) => comm.commentId == commentId
     );
 
-    if (comment[0].likes.includes(userId)) {
-      return res.json({ msg: "You can not like a comment twice" });
+    if (!comment) {
+      throw new Error("No comment found");
+    }
+
+    if (comment[0].likes.includes(likeById)) {
+      return res.status(405).json({ msg: "You can not like a comment twice" });
     }
 
     const artisan = await Artisan.findOneAndUpdate(
@@ -54,12 +80,15 @@ const addLikes = async (req, res) => {
       },
       {
         $push: {
-          "comments.$.likes": userId,
+          "comments.$.likes": likeById,
         },
       },
       { new: true, runValidators: true }
     );
-    res.json({ msg: "you liked a comment" });
+    res.status(200).json({ msg: "you liked a comment" });
+    if (!artisan) {
+      throw new Error("No Artisan found");
+    }
   } catch (error) {
     next(error);
   }
--- routers/user.js
+++ routers/user.js
@@ -12,9 +12,9 @@ const {
 const router = express.Router();
 
 router.get("/", getAllUsers);
-router.post("/comment/:artisanId/:userId", authentication, addComment);
+router.post("/comment/:artisanId", authentication, addComment);
 router.patch("/rating/:artisanId/:userId", authentication, addrating);
-router.patch("/like/:commentId/:userId", authentication, addLikes);
+router.patch("/like/:commentId", authentication, addLikes);
 router.patch("/unlike/:commentId/:userId", authentication, unLike);
 
 module.exports = router;
