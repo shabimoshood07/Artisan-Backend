fcaf6f7c85b766a3bdcc06c20bb4e7347cadff51
Moshood Olawale <shabimoshood07@gmail.com>
2023-03-24 13:46:30 +0100

    progress

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["https://searchartisans.netlify.app/", "http://localhost:3000"],
+    origin: ["https://searchartisans.netlify.app", "http://localhost:3000"],
   })
 );
 // app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
