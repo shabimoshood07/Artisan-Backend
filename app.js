8cc89d8d334e76853225e628a6a67b13bc07ca83
Moshood Olawale <shabimoshood07@gmail.com>
2023-03-24 13:42:31 +0100

    progress

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const authRouter = require("./routers/auth");
 const userRouter = require("./routers/user");
 const artisanRouter = require("./routers/artisan");
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({ credentials: true }));
 // app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
